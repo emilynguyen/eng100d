a792345c5ef1d5ceb2c832f174b8a409b9bacd82
Kevin Gong <kgong@ucsd.edu>
2018-05-23 19:32:23 -0700

    created database with questions from json file

--- create_database.js
+++ create_database.js
@@ -1,22 +1,15 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('assessment.db');
-const questions = require('./testQuestions.json');
+const questions = require('./questions.json');
 
 db.serialize(() => {
-  db.run("CREATE TABLE assessmentTable (testing TEXT)");
-  /*
-  db.run("ALTER TABLE assessmentTable ADD Does the retailer sell tobacco products TEXT");
-  db.run("ALTER TABLE assessmentTable ADD Tobacco license clearly displayed TEXT");
-  */
+  db.run("CREATE TABLE assessmentTable (date TEXT)");
+  //db.run("ALTER TABLE assessmentTable ADD Does the retailer sell tobacco products TEXT");
 
   for(const q of questions){
-    const myTitle = q.title;
-    console.log(myTitle);
+    const myTitle = '"' + q.title + '"';
+    //console.log(myTitle);
     db.run(`ALTER TABLE assessmentTable ADD ${myTitle}`);
   }
-  /*
-  db.each("SELECT * FROM assessmentTable", (err,row) => {
-    console.log(row);
-  });*/
 });
 db.close();
